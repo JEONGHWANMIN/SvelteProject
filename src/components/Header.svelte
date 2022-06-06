ca67500e4c1cff245f81d9ef66fe416100868b31
JEONGHWANMIN <mimi0j0j@naver.com>
2022-06-06 17:38:48 +0900

    Feat : "추가 Header 컴포넌트"

---
+++ src/components/Header.svelte
@@ -0,0 +1,17 @@
+<nav>
+  <h1>logo</h1>
+  <ul>
+    <li>로그인</li>
+    <li>로그아웃</li>
+  </ul>
+</nav>
+
+<style lang="scss">
+  nav{
+    width: 100%;
+    height: 100%;
+    background-color: aqua;
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
