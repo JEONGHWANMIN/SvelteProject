96fdcb55052ccf3bca053a670aa2eac6c879d95a
JEONGHWANMIN <mimi0j0j@naver.com>
2022-06-07 03:10:23 +0900

    Feat : "추가 menubar"

---
+++ src/components/MenuHeader.svelte
@@ -0,0 +1,35 @@
+
+
+<div class="MenuBar">
+  <ul>
+    <li>Monitor</li>
+    <li>Mouse</li>
+    <li>Chair</li>
+    <li>LabTop</li>
+    <li>Mug Cup</li>
+    <li>Earphone</li>
+    <li>LabTop Stand</li>
+    <li>Tea</li>
+  </ul>
+</div>
+
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Inconsolata:wght@500&display=swap');
+  .MenuBar {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: 'Inconsolata', monospace;
+    font-size: 1.5rem;
+    color: #555;
+
+/* ul , li */
+    ul {
+      display: flex;
+      li {
+        list-style: none;
+        margin: 1rem;
+      }
+    }
+  }
+</style>
