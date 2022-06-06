41c4e8474711ce8e399ec76fec2c85e5ef4bbecb
JEONGHWANMIN <mimi0j0j@naver.com>
2022-06-06 18:33:04 +0900

    Feat : "각각 pages 경로 연결 "

---
+++ src/Router.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Home from "./pages/Home.svelte";
+  import Login from "./pages/Login.svelte";
+  import SignUp from "./pages/SignUp.svelte";
+  import { Router, Route} from "svelte-navigator";
+  import Header from "./components/Header.svelte";
+</script>
+
+
+<Router>
+    <Header />
+    <Route path="/" component={Home} />
+    <Route path="/login" component={Login} />
+    <Route path="/signup" component={SignUp} />
+</Router>
