47f0cec8f3b0398f9528d589cb74175391013b35
JEONGHWANMIN <mimi0j0j@naver.com>
2022-06-06 17:09:28 +0900

    Fix: "Scss 적용시 오류 문구 수정"
    * @ts-ignore로 추가

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import App from './App.svelte'
+import './app.scss';
+// @ts-ignore
+import App from './App.svelte';
 
 const app = new App({
-  target: document.getElementById('app')
-})
+  target: document.getElementById('app'),
+});
 
-export default app
+export default app;
