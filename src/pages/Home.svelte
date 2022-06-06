36ed1b70e436ecef174e645cf0414c24f86196c8
JEONGHWANMIN <mimi0j0j@naver.com>
2022-06-07 03:10:54 +0900

    Feat : "추가 Banner 로직"

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,5 +1,34 @@
 <script>
+ import Banners1 from '../images/banners/banners1.jpg'
+ import Banners2 from '../images/banners/banners2.jpg'
+ import Banners3 from '../images/banners/banners3.jpg'
+
+ let Banners = [Banners1 ,Banners2,Banners3]
+ let num = 0
+ setInterval(() => {
+   if (num === 2) num = 0
+   else  num = num + 1
+   console.log(num)
+ }, 3000);
  
 </script>
 
-<h1>Home Page</h1>
+<div class="BannersBox">
+  <img src={Banners[num]} alt="Shopa">
+</div>
+
+<style lang="scss">
+  .BannersBox {
+    transition-delay: 10s;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 70%;
+    margin: auto;
+    background-color: lightgrey;
+    img {
+      height: 50rem;
+    }
+  }
+  
+</style>
