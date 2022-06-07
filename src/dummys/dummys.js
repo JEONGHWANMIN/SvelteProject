1857db06ea53678020cccfc8b84f409a6a012179
JEONGHWANMIN <mimi0j0j@naver.com>
2022-06-07 10:13:00 +0900

    Feat : "추가 더미 데이터"

---
+++ src/dummys/dummys.js
@@ -0,0 +1,26 @@
+export const dummys = [
+  {
+    name: '삼성전자 C27RG50',
+    img: 'https://file.bodnara.co.kr/logo/insidelogo.php?image=%2Fhttps%3A%2F%2Ffile.bodnara.co.kr%2Fwebedit%2Fhardward%2Fmonitor%2Falllook_guide%2Fmonitor.jpg',
+    content: '240hz 고주사율 모니터 !! ',
+    price: '2400000',
+  },
+  {
+    name: '삼성전자 C27RG50',
+    img: 'https://file.bodnara.co.kr/logo/insidelogo.php?image=%2Fhttps%3A%2F%2Ffile.bodnara.co.kr%2Fwebedit%2Fhardward%2Fmonitor%2Falllook_guide%2Fmonitor.jpg',
+    content: '240hz 고주사율 모니터 !! ',
+    price: '2400000',
+  },
+  {
+    name: '삼성전자 C27RG50',
+    img: 'https://file.bodnara.co.kr/logo/insidelogo.php?image=%2Fhttps%3A%2F%2Ffile.bodnara.co.kr%2Fwebedit%2Fhardward%2Fmonitor%2Falllook_guide%2Fmonitor.jpg',
+    content: '240hz 고주사율 모니터 !! ',
+    price: '2400000',
+  },
+  {
+    name: '삼성전자 C27RG50',
+    img: 'https://file.bodnara.co.kr/logo/insidelogo.php?image=%2Fhttps%3A%2F%2Ffile.bodnara.co.kr%2Fwebedit%2Fhardward%2Fmonitor%2Falllook_guide%2Fmonitor.jpg',
+    content: '240hz 고주사율 모니터 !! ',
+    price: '2400000',
+  },
+];
