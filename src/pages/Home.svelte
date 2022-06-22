893166cfbd32b64b01cba275143d517c9007e88f
JEONGHWANMIN <mimi0j0j@naver.com>
2022-06-22 16:52:11 +0900

    Feat : "상품 데이터 API 연동 test"

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,51 +1,60 @@
 <script>
- import Banners1 from '../images/banners/banners1.jpg'
- import Banners2 from '../images/banners/banners2.jpg'
- import Banners3 from '../images/banners/banners3.jpg'
- import Card from '../components/Card.svelte'
- import {dummys} from '../dummys/dummys'
- 
-let BannerNum = 0
-setInterval(() => {
-  BannerNum === 4 ? BannerNum = 1 : BannerNum++
-  
-}, 4000);
+  import Banners1 from '../images/banners/banners1.jpg';
+  import Banners2 from '../images/banners/banners2.jpg';
+  import Banners3 from '../images/banners/banners3.jpg';
+  import Card from '../components/Card.svelte';
+  import { dummys } from '../dummys/dummys';
+  import { getProduct } from '../apis/apiClient';
 
+  // Banner API
+  let BannerNum = 0;
+  setInterval(() => {
+    BannerNum === 4 ? (BannerNum = 1) : BannerNum++;
+  }, 4000);
+  // Product API
+  const productData = getProduct();
+</script>
 
-
- </script>
-
+<!-- Home Banner -->
 <div class="hidden">
-  <div class="BannersBox" style={BannerNum === 1 ? 'transform: translate(0vw)' : BannerNum === 2 ? 'transform: translate(-100vw)' : 'transform: translate(-200vw)'}>
+  <div
+    class="BannersBox"
+    style={BannerNum === 1
+      ? 'transform: translate(0vw)'
+      : BannerNum === 2
+      ? 'transform: translate(-100vw)'
+      : 'transform: translate(-200vw)'}
+  >
     <div class="innerBox">
-      <img src={Banners1} alt="Shopa">
+      <img src={Banners1} alt="Shopa" />
     </div>
     <div class="innerBox">
-      <img src={Banners2} alt="Shopa">
+      <img src={Banners2} alt="Shopa" />
     </div>
     <div class="innerBox">
-      <img src={Banners3} alt="Shopa">
+      <img src={Banners3} alt="Shopa" />
     </div>
   </div>
   <div class="banner-btns">
-    <button class="banner-btn" on:click={() => BannerNum = 1}></button>
-    <button class="banner-btn" on:click={() => BannerNum = 2}></button>
-    <button class="banner-btn" on:click={() => BannerNum = 3}></button>
+    <button class="banner-btn" on:click={() => (BannerNum = 1)} />
+    <button class="banner-btn" on:click={() => (BannerNum = 2)} />
+    <button class="banner-btn" on:click={() => (BannerNum = 3)} />
   </div>
 </div>
 
-
-
+<!-- Products -->
 <div class="CardContainer">
-  {#each dummys.slice(0,3) as dummy}
-    <Card name={dummy.name} price={dummy.price} img={dummy.img} content={dummy.content} />
+  {#each dummys.slice(0, 3) as dummy}
+    <Card
+      name={dummy.name}
+      price={dummy.price}
+      img={dummy.img}
+      content={dummy.content}
+    />
   {/each}
 </div>
 
-
-
 <style lang="scss">
-  
   .hidden {
     overflow: hidden;
     position: relative;
@@ -88,5 +97,4 @@ setInterval(() => {
     width: 80%;
     margin: auto;
   }
-  
 </style>
