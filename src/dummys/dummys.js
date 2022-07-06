54e3bce3faf1151be3c008fedad591449d781e6c
JEONGHWANMIN <mimi0j0j@naver.com>
2022-07-06 13:57:26 +0900

    Feat : "추가 더미 데이터"

--- src/dummys/dummys.js
+++ src/dummys/dummys.js
@@ -1,26 +1,23 @@
 export const dummys = [
   {
+    category: 'monitor',
     name: '삼성전자 C27RG50',
     img: 'https://file.bodnara.co.kr/logo/insidelogo.php?image=%2Fhttps%3A%2F%2Ffile.bodnara.co.kr%2Fwebedit%2Fhardward%2Fmonitor%2Falllook_guide%2Fmonitor.jpg',
     content: '240hz 고주사율 모니터 !! ',
     price: '2400000',
   },
   {
-    name: '삼성전자 C27RG50',
-    img: 'https://file.bodnara.co.kr/logo/insidelogo.php?image=%2Fhttps%3A%2F%2Ffile.bodnara.co.kr%2Fwebedit%2Fhardward%2Fmonitor%2Falllook_guide%2Fmonitor.jpg',
-    content: '240hz 고주사율 모니터 !! ',
-    price: '2400000',
+    category: 'Etc',
+    name: '핑크퐁 아기상어 40 페이스 장우산 6K IUPFU10008',
+    img: 'https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/product/image/vendoritem/2019/05/02/4365228667/dad9add5-7819-4401-978b-284412d30d82.jpg',
+    content: '귀여운 상어 우산 !! ',
+    price: '32000',
   },
   {
-    name: '삼성전자 C27RG50',
-    img: 'https://file.bodnara.co.kr/logo/insidelogo.php?image=%2Fhttps%3A%2F%2Ffile.bodnara.co.kr%2Fwebedit%2Fhardward%2Fmonitor%2Falllook_guide%2Fmonitor.jpg',
-    content: '240hz 고주사율 모니터 !! ',
-    price: '2400000',
-  },
-  {
-    name: '삼성전자 C27RG50',
-    img: 'https://file.bodnara.co.kr/logo/insidelogo.php?image=%2Fhttps%3A%2F%2Ffile.bodnara.co.kr%2Fwebedit%2Fhardward%2Fmonitor%2Falllook_guide%2Fmonitor.jpg',
-    content: '240hz 고주사율 모니터 !! ',
-    price: '2400000',
+    category: 'Etc',
+    name: 'RF-R3485WS 스탠드형 리모컨선풍기',
+    img: 'https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/eb3a/672b17cf5bce30ebcb2d6fa2f6d41e5ab850bccd6d5caaa5cb1417431925.jpg',
+    content: '엄청난 바람 선풍기 !',
+    price: '15900',
   },
 ];
